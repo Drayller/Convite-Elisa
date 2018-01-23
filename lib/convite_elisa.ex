0830d3ceaecdafb5d41b0f3bc6d5beb7a41f9037
Drayller <enzo.hock@gmail.com>
2018-01-23 17:54:35 -0200

    lib/convite_elisa.ex has been modified

---
+++ lib/convite_elisa.ex
@@ -0,0 +1,34 @@
+defmodule ConviteElisa do
+
+  def start do
+    bem_vindo
+      get_name
+      voce_me_conhece
+      pergunta
+    end
+
+    def bem_vindo do
+      IO.puts "Bem vindo/a estranho/a! "
+    end
+
+    def get_name do
+      IO.gets("Qual é o seu nome? ")
+      |> String.trim
+    end
+
+    def voce_me_conhece do
+      IO.getn("Você conhece o Enzo? [y|n] ", 1)
+    end
+
+    def pergunta do
+      name = get_name
+      case voce_me_conhece do
+        "y" ->
+          IO.puts " Interessante #{name}. "
+          "n" ->
+            IO.puts "Que pena, deveria conhecer ele, ele é uma otima pessoa."
+            _  ->
+            IO.puts "use apenas 'y' ou 'n'."
+          end
+        end
+      end
